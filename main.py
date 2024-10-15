9082aad620b54f5eb92ae73fd2b4813169d5e31b
Luiz Victor Reis <102300332+luiz315@users.noreply.github.com>
2024-10-15 14:46:52 -0300

    Update main.py

--- main.py
+++ main.py
@@ -6,12 +6,12 @@ from pathlib import Path
 html = Template(Path('index.html').read_text())
 email = EmailMessage()
 email['from'] = 'Luiz Victor'
-email ['to'] = 'luizvictorreis315@gmail.com'
+email ['to'] = 'exmaple_email@gmail.com'
 email ['subject'] = ':) TOP DEmais, Seloko Fia  '
 email.set_content(html.substitute({'name':'TinTin'}),'html')
 with smtplib.SMTP(host='smtp.gmail.com', port=587) as smtp:
     smtp.ehlo()
     smtp.starttls()
-    smtp.login('vingadorestheavengers@gmail.com','fasu upxd qgpi nlew')
+    smtp.login('exmaple_email@gmail.com','fasu upxd qgpi nlew')
     smtp.send_message(email)
     print ('all good boss')
